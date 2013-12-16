d8ea182a12626f7d3825377377c4b670ad182cc6
Lidza Louina <lidza.louina@gmail.com>
2013-12-16 18:58:55 -0500

    Integrates yeoman-marionette server-side boilerplate with PosTopic
    This patch gets rid of adds some of the server-side code from
    PosTopic to the current yeoman-marionette build. The
    api routes are working and rendering json data.
    
    Signed-off-by: Lidza Louina <lidza.louina@gmail.com>

--- server/app.js
+++ server/app.js
@@ -5,29 +5,31 @@ var http = require('http');
 var path = require('path');
 var async = require('async');
 var hbs = require('express-hbs');
-var baucis = require('baucis');
-var socketIO = require('socket.io');
 var mongoose = require('mongoose');
+var db = require('./db');
 
+var post = require('./routes/post');
+var topic = require('./routes/topic');
+var index = require('./routes/index');
 
-// start mongoose
-mongoose.connect('mongodb://localhost/sit');
-var db = mongoose.connection;
+var Post = require('./models/Post.js');
+var Topic = require('./models/Topic.js');
+
+var routerData = {
+  database: {
+    model: {
+      post: Post,
+      topic: Topic
+    }
+  },
+  helper: {
+    async: async
+  }
+};
+
 
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function callback () {
-
-	/* test schema */
-    var testSchema = new mongoose.Schema({
-        test: String
-    });
-
-    var Test = mongoose.model( 'test', testSchema );
-
-    /* set Baucis */
-    baucis.rest({
-        singular: 'test'
-    });
 
 	var app = express();
 
@@ -38,7 +40,6 @@ db.once('open', function callback () {
 	    app.set('views', __dirname + '../app/scripts/views');
 	});
 
-    app.use('/api/v1', baucis());
 
 	// simple log
 	app.use(function(req, res, next){
@@ -52,9 +53,21 @@ db.once('open', function callback () {
 
 
 	// route index.html
-	app.get('/', function(req, res){
-	  res.sendfile( path.join( __dirname, '../app/index.html' ) );
-	});
+  app.get('/api/topics', topic.all(routerData));
+  app.get('/api/topics/:id', topic.id(routerData));
+  app.post('/api/topics', topic.create(routerData));
+  app.del('/api/topics/:id', topic.id.del(routerData));
+  app.del('/api/topics', topic.all.del(routerData));
+  app.put('/api/topics/:id', topic.id.update(routerData));
+
+  app.get('/api/posts', post.all(routerData));
+  app.get('/api/posts/:id', post.id(routerData));
+  app.post('/api/posts', post.create(routerData));
+  app.del('/api/posts/:id', post.id.del(routerData));
+  app.del('/api/posts', post.all.del(routerData));
+  app.put('/api/posts/:id', post.id.update(routerData));
+  
+  app.all('*', index.render());
 
 	// start server
 	http.createServer(app).listen(app.get('port'), function(){
