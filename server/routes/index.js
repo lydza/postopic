9ce8df1adfbba6f40d15dde0dd7d822f8277c205
Lidza Louina <Lidza.Louina@gmail.com>
2013-12-23 19:01:32 -0500

    Fixes routing issue and adds links to all posts and topics
    The routes previously needed to have hashes in order to be rendered.
    
    Signed-off-by: Lidza Louina <Lidza.Louina@gmail.com>

--- server/routes/index.js
+++ server/routes/index.js
@@ -9,6 +9,6 @@
 module.exports.render = function(){
   return function(req, res){
     console.log('\nHttp Method:   * \nRoute:         /* \nAction:        Goes to backbone app');
-    res.sendfile( path.join( __dirname, '../app/index.html' ) );
+    res.sendfile('app/index.html');
   }
 };
